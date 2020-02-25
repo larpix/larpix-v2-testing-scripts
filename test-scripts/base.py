bcc194b438f55363398fd4e5fd8da4f2b16d39af
Peter Madigan <pmadigan@berkeley.edu>
2020-02-25 11:42:43 -0800

    Add hard reset to base.py script

--- test-scripts/base.py
+++ test-scripts/base.py
@@ -2,7 +2,7 @@ import larpix
 import larpix.io
 import larpix.logger
 
-def set_uart_clk_ctrl(controller, clk_ctrl):
+def set_uart_clk_ctrl(controller, chip_key, clk_ctrl):
     '''
     Updates the uart clock speed for the system using the broadcast id
 
@@ -13,13 +13,8 @@ def set_uart_clk_ctrl(controller, clk_ctrl):
     3 = 16 clk cycles / bit
 
     '''
-    config_packet = larpix.Packet_v2()
-    config_packet.chip_key = '1-1-255'
-    config_packet.packet_type = 2
-    config_packet.register_address = 123
-    config_packet.register_data = 8
-    config_packet.assign_parity()
-    controller.io.send([config_packet])
+    controller[chip_key].config.clk_ctrl = clk_ctrl
+    controller.write_configuration(chip_key, 123) # clk_ctrl reg
     clk_ctrl_2_clk_ratio_map = {
         0: 2,
         1: 4,
@@ -27,7 +22,7 @@ def set_uart_clk_ctrl(controller, clk_ctrl):
         3: 16}
     c.set_larpix_uart_clk_ratio(clk_ctrl_2_clk_ratio_map[clk_ctrl])
 
-def main(logger=False, soft_reset=True):
+def main(logger=False, reset=True):
     print('base config')
 
     # create controller
@@ -40,18 +35,14 @@ def main(logger=False, soft_reset=True):
 
     c.add_chip('1-1-1')
 
-    if soft_reset:
-        # assumes uart clk ctrl of 1 (4 cycles / bit)
-        print('soft reset')
-        set_uart_clk_ctrl(c, 1)
-        c['1-1-1'].config.load_config_defaults = 1
-        c.write_configuration('1-1-1',[123])
-        c['1-1-1'].config.load_config_defaults = 0
-        c.write_configuration('1-1-1',[123])
-        set_uart_clk_ctrl(c, 0)
+    if reset:
+        # issues hard reset to larpix
+        c.io.set_larpix_reset_cnt(128)
+        c.io.larpix_reset()
+        c.io.set_larpix_uart_clk_ratio(1)
 
     # set uart speed
-    set_uart_clk_ctrl(c, 1)
+    set_uart_clk_ctrl(c, '1-1-1', 1)
 
     # set configuration
     c['1-1-1'].config.enable_miso_upstream[0] = 1
