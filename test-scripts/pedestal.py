80c3ef2f5dc4f42b9635dde89b2ddcb210417501
Peter Madigan <pmadigan@berkeley.edu>
2020-03-05 14:32:04 -0800

    Update pedestal.py to run with N chips

--- test-scripts/pedestal.py
+++ test-scripts/pedestal.py
@@ -1,30 +1,34 @@
+import sys
+
 import larpix
 import larpix.io
 
 import base
 import base_warm
 
-def main():
+def main(controller_config=None):
     # create controller
-    c = base_warm.main(logger=True)
+    c = base_warm.main(controller_config_file=controller_config, logger=True)
 
     # set configuration
-    c['1-1-1'].config.periodic_trigger_mask = [0]*64
-    c['1-1-1'].config.channel_mask = [0]*64
-    c['1-1-1'].config.periodic_trigger_cycles = 100000 # 100Hz
-    c['1-1-1'].config.enable_periodic_trigger = True
-    c['1-1-1'].config.enable_rolling_periodic_trigger = True
-    c['1-1-1'].config.enable_periodic_reset = True
-    c['1-1-1'].config.enable_hit_veto = False
+    for chip_key, chip in c.chips.items():
+        chip.config.periodic_trigger_mask = [0]*64
+        chip.config.channel_mask = [0]*64
+        chip.config.periodic_trigger_cycles = 100000 # 100Hz
+        chip.config.enable_periodic_trigger = True
+        chip.config.enable_rolling_periodic_trigger = True
+        chip.config.enable_periodic_reset = True
+        chip.config.enable_hit_veto = False
 
-    # write configuration
-    registers = list(range(155,163)) # periodic trigger mask
-    registers += list(range(131,139)) # channel mask
-    registers += list(range(166,170)) # periodic trigger cycles
-    registers += [128] # periodic trigger, reset, rolling trigger, hit veto
-    c.write_configuration('1-1-1', registers)
+        # write configuration
+        registers = list(range(155,163)) # periodic trigger mask
+        registers += list(range(131,139)) # channel mask
+        registers += list(range(166,170)) # periodic trigger cycles
+        registers += [128] # periodic trigger, reset, rolling trigger, hit veto
 
-    ok, diff = c.verify_configuration('1-1-1')
+        c.write_configuration(chip_key, registers)
+
+    ok, diff = c.verify_configuration()
     if not ok:
         print('config error',diff)
 
@@ -37,4 +41,4 @@ def main():
     return c
 
 if __name__ == '__main__':
-    c = main()
+    c = main(*sys.argv[1:])
