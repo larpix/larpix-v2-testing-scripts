666e3001732346b3555f1924c00cd8ddba5c1171
Peter Madigan <pmadigan@berkeley.edu>
2020-03-05 14:16:19 -0800

    Modify base.py to run without a config file

--- test-scripts/base.py
+++ test-scripts/base.py
@@ -24,6 +24,14 @@ def main(controller_config_file=None, logger=False, reset=True):
 
     if controller_config_file is None:
         c.add_chip('1-1-1')
+        # create node for fpga
+        c.add_network_node(1, 1, c.network_names, 'ext', root=True)
+        # create upstream link on uart 0
+        c.add_network_link(1,1,'miso_us',('ext',1),0)
+        # create downstream link on uart 0
+        c.add_network_link(1,1,'miso_ds',(1,'ext'),0)
+        # create mosi link
+        c.add_network_link(1,1,'mosi',('ext',1),0)
     else:
         c.load(controller_config_file)
 
@@ -36,9 +44,9 @@ def main(controller_config_file=None, logger=False, reset=True):
     for io_group, io_channels in c.network.items():
         for io_channel in io_channels:
             c.init_network(io_group, io_channel)
-            ok, diff = c.verify_network()
+            ok, diff = c.verify_network(c.get_network_keys(io_group, io_channel))
             if not ok:
-                print('network'io_group,io_channel,'config error',diff)
+                print('network',io_group,io_channel,'config error',diff)
             else:
                 print('network',io_group,io_channel,'configured ok')
 
@@ -67,7 +75,7 @@ def main(controller_config_file=None, logger=False, reset=True):
         c.write_configuration(chip_key, registers)
 
     # verify
-    ok, diff = c.verify_configuration('1-1-1')
+    ok, diff = c.verify_configuration()
     if not ok:
         print('config error',diff)
     else:
