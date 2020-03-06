99ce4395cf43555874e559436b4340e32b8ae678
Peter Madigan <pmadigan@berkeley.edu>
2020-03-05 16:16:37 -0800

    Update internal pulse to run on N chips

--- test-scripts/internal_pulse.py
+++ test-scripts/internal_pulse.py
@@ -12,42 +12,59 @@ import larpix.logger
 import base_warm
 import load_config
 
-def main(config_name=None, channels=range(0,64,1), pulse_dac=10, n_pulses=10):
+import sys
+
+def main(config_name=None, controller_config=None, pulse_dac=10, n_pulses=10, channels=range(0,64,1), runtime=0.01):
     print('configure thresholds')
+    pulse_dac = int(pulse_dac)
+    n_pulses = int(n_pulses)
 
     # create controller
     c = None
     if config_name is None:
-        c = base_warm.main(logger=True)
+        c = base_warm.main(controller_config, logger=True)
     else:
-        c = load_config.main(config_name, logger=True)
+        if controller_config is None or controller_config == 'None':
+            c = load_config.main(config_name, logger=True)
+        else:
+            c = load_config.main(config_name, controller_config, logger=True)
 
     # set initial configuration
     print('channels',channels)
     print('pulse_dac', pulse_dac)
     print('n_pulses', n_pulses)
 
-    c.['1-1-1'].config.adc_hold_delay = 15
-    registers = [129]
-    c.write_configuration('1-1-1', registers)
+    for chip, chip_key in c.chips.items():
+        chip.config.adc_hold_delay = 15
+        registers = [129]
+        c.write_configuration(chip_key, registers)
 
-    c.enable_testpulse('1-1-1',channels)
+        c.enable_testpulse(chip_key, channels)
 
     # verify
-    ok, diff = c.verify_configuration('1-1-1')
+    ok, diff = c.verify_configuration()
     if not ok:
         print('config error',diff)
+    else:
+        print('config ok')
+
+    # add a dummy for chip 255
+    for io_group, io_channels in c.network.items():
+        for io_channel in io_channels:
+            c.add_chip(Key(io_group,io_channel,255))
 
     # issue pulses
     for i in range(n_pulses):
-        c.logger.enable()
-        c.issue_testpulse(pulse_dac)
-        c.logger.disable()
+        for io_group, io_channels in c.network.items():
+            for io_channel in io_channels:
+                c.logger.enable()
+                c.issue_testpulse(Key(io_group,io_channel,255),pulse_dac,read_time=runtime)
+                c.logger.disable()
 
-        print('pulse',i,'triggers',len(c.reads[-1]))
+                print('pulse',i,'triggers',len(c.reads[-1]))
 
-        c.enable_testpulse('1-1-1',channels)
-        c.run(0.1,'flush stale data')
+                c.enable_testpulse(Key(io_group,io_channel,255),channels)
+                c.run(runtime,'flush stale data')
 
     return c
 
