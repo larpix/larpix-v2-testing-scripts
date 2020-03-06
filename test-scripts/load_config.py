68e9015097b148a4582964a0415e69f80fb07c66
Peter Madigan <pmadigan@berkeley.edu>
2020-03-05 16:07:00 -0800

    Update load config to run on N chips

--- test-scripts/load_config.py
+++ test-scripts/load_config.py
@@ -7,12 +7,16 @@ Usage:
 '''
 
 import sys
+import os
+import glob
 
 import larpix
 import larpix.io
 import larpix.logger
 
 import base
+
+config_format = 'config-{chip_key}-*.json$'
 
 def main(config_name, *args, **kwargs):
     print('load config')
@@ -21,13 +25,22 @@ def main(config_name, *args, **kwargs):
     c = base.main(*args, **kwargs)
 
     # set configuration
-    c['1-1-1'].config.load(config_name)
+    if not os.isdir(config_name):
+        for chip_key,chip in c.chips.items():
+            chip.config.load(config_name)
+    else:
+        # load all configurations for chips
+        for chip_key,chip in c.chips.items():
+            config_files = glob.glob(os.join(config_name, config_format.format(chip_key=chip_key)))
+            if config_files:
+                chip.config.load(sorted(config_files[-1]))
 
     # write configuration
-    c.write_configuration('1-1-1')
+    for chip_key, chip in c.chips.items():
+        c.write_configuration(chip_key)
 
     # verify
-    ok, diff = c.verify_configuration('1-1-1')
+    ok, diff = c.verify_configuration()
     if not ok:
         print('config error',diff)
 
