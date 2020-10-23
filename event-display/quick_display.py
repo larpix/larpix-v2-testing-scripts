bd9be2e7cb49149118e53e2e339d79312fcd3586
peter-madigan <pmadigan@berkeley.edu>
2020-10-23 11:53:26 -0700

    Fix ntracks check, make voxel plot optional

--- event-display/quick_display.py
+++ event-display/quick_display.py
@@ -44,15 +44,21 @@ def vol3d(x,y,z,q,*geom,name=None,fig=None,points=False):
     plt.draw()
     return fig
 
-def line3d(x,y,z,*geom,name=None,fig=None,points=False):
+def line3d(x,y,z,*geom,name=None,fig=None,points=False,c=None,edgecolors=None):
     xyz = np.array(list(zip(x,y,z)))
 
-    # ax = fig.add_subplot('122', projection='3d')
-    ax = fig.gca()
-    if not points:
-        ax.plot(x,y,z,alpha=1)
+    if fig:
+        axes = fig.get_axes()
+        ax = axes[-1] if axes else fig.add_subplot(1,2,2, projection='3d')
     else:
-        ax.scatter(xyz[:,0],xyz[:,1],xyz[:,2],s=1,alpha=0.75)
+        ax = fig.add_subplot(1,2,2, projection='3d')
+    if not points:
+        ax.plot(x,y,z,alpha=1,color=c)
+    else:
+        norm = lambda x: np.clip((x - max(np.min(x),0.001)) / (np.max(x) - max(np.min(x),0.001)),0,1)
+        cmap = plt.cm.get_cmap('plasma')
+        edgecolors = cmap(norm(edgecolors))
+        ax.scatter(xyz[:,0],xyz[:,1],xyz[:,2],s=5,alpha=0.75,c=c,edgecolors=edgecolors)
     ax.set_xlabel('x [mm]')
     ax.set_ylabel('y [mm]')
     ax.set_zlabel('t [0.1us]')
@@ -63,11 +69,11 @@ def line3d(x,y,z,*geom,name=None,fig=None,points=False):
     plt.draw()
     return fig
 
-def line2d(x,y,*geom,name=None,fig=None):
+def line2d(x,y,*geom,name=None,fig=None,color=None):
     xy = np.array(list(zip(x,y)))
 
     ax = fig.gca()
-    ax.plot(x,y)
+    ax.plot(x,y,color=color)
     plt.tight_layout()
 
     plt.draw()
@@ -119,7 +125,7 @@ def hit_times(t,q,*geom,name=None,fig=None):
     plt.draw()
     return fig
 
-def generate_plots(event, f, geom=[], fig=None):
+def generate_plots(event, f, geom=[], fig=None, binned_3d=False):
     name = 'Event {}/{} ({})'.format(event['evid'],len(f['events']),f.filename)
 
     hits = f['hits']
@@ -133,23 +139,33 @@ def generate_plots(event, f, geom=[], fig=None):
     z = hits[hit_ref]['ts'] - event['ts_start']
     q = hits[hit_ref]['q'] * 0.250
 
-    if tracks and 'ntracks' in event:
+    if not fig:
+        fig = plt.figure(name,figsize=(8,6))
+        
+    if binned_3d:
+        fig = vol3d(x,y,z,q,*geom,name=name,fig=fig)
+    if tracks and 'ntracks' in event.dtype.names and event['ntracks']:
         track_start = tracks[track_ref]['start'][:,[0,1,3]]
         track_end = tracks[track_ref]['end'][:,[0,1,3]]
-
-    if not fig:
-        fig = plt.figure(name)
-    fig = vol3d(x,y,z,q,*geom,name=name,fig=fig)
-    if tracks and 'ntracks' in event:
-        for s,e in zip(track_start,track_end):
-            fig = line3d((s[0],e[0]),(s[1],e[1]),(s[2],e[2]),*geom,name=name,fig=fig)
-        for track in tracks[track_ref]:
+        unassoc_hit_mask = np.ones(event['nhit']).astype(bool)
+        for i,track in enumerate(tracks[track_ref]):
             hit_ref = track['hit_ref']
-            fig = line3d(hits[hit_ref]['px'],hits[hit_ref]['py'],hits[hit_ref]['ts']-event['ts_start'],*geom,name=name,fig=fig,points=True)
+            fig = line3d(hits[hit_ref]['px'], hits[hit_ref]['py'], hits[hit_ref]['ts']-event['ts_start'], *geom, edgecolors=hits[hit_ref]['q'], c='C{}'.format(i+1), name=name, fig=fig, points=True)
+            fig = line3d((track_start[i,0],track_end[i,0]), (track_start[i,1],track_end[i,1]), (track_start[i,2],track_end[i,2]), *geom, name=name, fig=fig, c='C{}'.format(i+1))
+            unassoc_hit_mask[np.in1d(hits[event['hit_ref']]['hid'],hits[hit_ref]['hid'])] = 0
+        if np.sum(unassoc_hit_mask):
+            unassoc_hits = hits[event['hit_ref']][unassoc_hit_mask]
+            fig = line3d(unassoc_hits['px'], unassoc_hits['py'], unassoc_hits['ts']-event['ts_start'], *geom, edgecolors=unassoc_hits['q'], c='C0', name=name, fig=fig, points=True)
+    else:
+        unassoc_hits = hits[hit_ref]
+        fig = line3d(unassoc_hits['px'], unassoc_hits['py'], unassoc_hits['ts']-event['ts_start'], *geom, edgecolors=unassoc_hits['q'], name=name, fig=fig, points=True, c='C0')
+        
     fig = proj2d(x,y,q,*geom,name=name,fig=fig)
-    if tracks and 'ntracks' in event:
-        for s,e in zip(track_start,track_end):
-            fig = line2d((s[0],e[0]),(s[1],e[1]),*geom,name=name,fig=fig)
+    if tracks and 'ntracks' in event.dtype.names and event['ntracks']:
+        for i,se in enumerate(zip(track_start,track_end)):
+            s,e = se
+            fig = line2d((s[0],e[0]),(s[1],e[1]),*geom,name=name,fig=fig, color='C{}'.format(i+1))
+            
     fig = proj_time(z,q,*geom,name=name,fig=fig)
     fig = hit_times(z,q,*geom,name=name,fig=fig)
     fig.canvas.set_window_title(name)
@@ -171,9 +187,9 @@ def main(args):
             sys.exit()
         event = events[events['nhit'] > args.nhit_sel][ev]
         print('Event:',event)
-        if tracks and 'ntracks' in event: print('Track:',tracks[event['track_ref']])
+        if tracks and 'ntracks' in event.dtype.names and event['ntracks']: print('Track:',tracks[event['track_ref']])
         print('Hits:',hits[event['hit_ref']])
-        fig = generate_plots(event, f, args.geom_limits, fig=fig)
+        fig = generate_plots(event, f, args.geom_limits, fig=fig, binned_3d=args.binned_3d)
         user_input = input('Next event (q to exit/enter for next/number to skip to position)?\n')
         print(user_input)
         if not len(user_input) or user_input[0] == '':
@@ -194,6 +210,9 @@ if __name__ == '__main__':
     ''')
     parser.add_argument('--geom_limits', default=[-159.624,159.624,-159.624,159.624,0,1900,4.434,23], nargs=8, type=float, metavar=('XMIN','XMAX','YMIN','YMAX','TMIN','TMAX','PIXEL_PITCH','TIME_VOXEL'), help='''
     Optional, limits for geometry
+    ''')
+    parser.add_argument('--binned_3d',action='store_true',default=False,help='''
+    Optional, use a 3D voxelization of charge in the 3D view (slower)
     ''')
     args = parser.parse_args()
     main(args)
