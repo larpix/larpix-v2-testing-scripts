16227848ecf9ff4fd2aebd79767219f8375e9c5d
Peter Madigan <pmadigan@berkeley.edu>
2021-04-02 10:51:32 -0700

    Swap slice / mask order

--- event-display/to_evd_file.py
+++ event-display/to_evd_file.py
@@ -31,7 +31,7 @@ def split_at_timestamp(timestamp,event,*args):
     Breaks event into two arrays at index where event['timestamp'] > timestamp
     Additional arrays can be specified with kwargs and will be split at the same
     index
-    
+
     :returns: tuple of two event halves followed by any additional arrays (in pairs)
     '''
     args = list(args)
@@ -120,7 +120,7 @@ def main(in_filename, out_filename, *args,
     del trigger_packet_mask
     del sync_packet_mask
 
-    n_packets      = int(np.sum(mask))
+    n_packets      = #int(np.sum(mask))
     start_idx      = 0
     end_idx        = buffer_size
     event_buffer   = np.array([],dtype=packets.dtype)
@@ -130,9 +130,18 @@ def main(in_filename, out_filename, *args,
     del timestamp_packet_mask
     while start_idx < n_packets and (max_packets < 0 or start_idx < max_packets):
         # load a buffer of data
-        packet_buffer = np.copy(packets[mask][start_idx:min(end_idx,n_packets)])
+        block = packets[start_idx:min(end_idx,n_packets)]
+
+        mask = (block['valid_parity'].astype(bool) & (block['packet_type'] == 0)) # data packets
+        mask = mask | (block['packet_type'] == 4) # timestamp packets
+        mask = mask | ((block['packet_type'] == 7) \
+            & 'pacman_trigger_enabled' in external_trigger_conf \
+            & external_trigger_conf['pacman_trigger_enabled']) # external trigger packets
+
+        packet_buffer = np.copy(block[mask])
+        # packet_buffer = np.copy(packets[mask][start_idx:min(end_idx,n_packets)])
         packet_buffer = np.insert(packet_buffer, [0], last_unix_ts)
-        
+
         # find unix timestamp groups
         ts_mask = packet_buffer['packet_type'] == 4
         ts_grps = np.split(packet_buffer, np.argwhere(ts_mask).flatten())
@@ -155,7 +164,7 @@ def main(in_filename, out_filename, *args,
         event_idx     = np.argwhere(np.abs(packet_dt) > event_dt).flatten()
         events        = np.split(packet_buffer, event_idx)
         event_unix_ts = np.split(unix_ts, event_idx)
-        
+
         for idx, event, unix_ts in zip(event_idx, events[:-1], event_unix_ts[:-1]):
             if idx == 0:
                 while len(event_buffer) >= nhit_cut:
